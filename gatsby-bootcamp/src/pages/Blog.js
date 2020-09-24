273b5975a432f0cf354d282be50c1573e7fe8d4e
antMaks <maksimantonuk74@gmail.com>
2020-09-24 15:31:37 +0300

    code

---
+++ gatsby-bootcamp/src/pages/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <>
+            <h1>Blog</h1>
+            <p>new blog</p>
+        </>
+    )
+}
+
+
+export default BlogPage
